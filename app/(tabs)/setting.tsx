b1a9268cfbf7d4a57dcaf7624bf0778bc466334b
Ohm0025 <toyohm0025@gmail.com>
2025-01-01 22:53:45 +0700

    set:base-component

---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Settings() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings tab</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
